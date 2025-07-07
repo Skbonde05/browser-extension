b65cad66c44d73e51fbdde5c648fce5aebc458d2
Akash Kumar <154402631+akash-kumar-dev@users.noreply.github.com>
2025-07-07 18:44:06 +0000

    fix: robust WebSocket reconnect in background script Add a reconnect loop to auto-reconnect if diconnected (eg. connection lost)

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,28 +1,51 @@
-import { closeWebSocket, initializeWebSocket } from "../services/websocket";
+import { closeWebSocket, initializeWebSocket, isWebSocketConnected } from "../services/websocket";
 import { tabTracing } from "../services/tabTracking";
 
 console.log('Background script loaded');
 
 tabTracing();
 
+let reconnectInterval: NodeJS.Timeout | null = null;
+
+function startReconnectLoop() {
+  if (reconnectInterval) return;
+  reconnectInterval = setInterval(() => {
+    if (!isWebSocketConnected()) {
+      console.log('[Background] Attempting to reconnect WebSocket...');
+      initializeWebSocket();
+    }
+  }, 15000);
+}
+
+function stopReconnectLoop() {
+  if (reconnectInterval) {
+    clearInterval(reconnectInterval);
+    reconnectInterval = null;
+  }
+}
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
   initializeWebSocket();
+  startReconnectLoop();
 });
 
 chrome.runtime.onStartup.addListener(() => {
   console.log('browser started');
   initializeWebSocket();
+  startReconnectLoop();
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'LOGIN_SUCCESS'){
     initializeWebSocket();
+    startReconnectLoop();
   } else if (message.type === 'LOGOUT') {
     closeWebSocket();
+    stopReconnectLoop();
   } else if (message.type === 'POPUP_OPENED') {
     initializeWebSocket();
+    startReconnectLoop();
   }
-
   return true;
 });
